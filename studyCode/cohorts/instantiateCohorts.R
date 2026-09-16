2540048514dc3852aa5eafc4af64970e7293db1c
Kim López Güell <99121045+KimLopezGuell@users.noreply.github.com>
2026-09-16 12:25:14 +0100

    add mi event criteria

--- studyCode/cohorts/instantiateCohorts.R
+++ studyCode/cohorts/instantiateCohorts.R
@@ -1,14 +1,34 @@
 # Load and validate the supplied concept sets ----
 source(here("codelist", "codelistCreation.R"))
 
-# MI target events ----
-cdm$mi_events <- CohortConstructor::conceptCohort(
+# All MI events used to apply the target-cohort eligibility criteria ----
+cdm$all_mi_events <- CohortConstructor::conceptCohort(
   cdm = cdm,
   conceptSet = mi_concept_set,
-  name = "mi_events",
+  name = "all_mi_events",
   table = "condition_occurrence",
   exit = "event_start_date"
 )
+
+# Eligible MI target events ----
+cdm$mi_events <- CohortConstructor::copyCohorts(
+  cohort = cdm$all_mi_events,
+  name = "mi_events"
+) |>
+  CohortConstructor::requireAge(ageRange = c(18, Inf)) |>
+  CohortConstructor::requireInDateRange(
+    dateRange = as.Date(c("2012-01-01", NA))
+  ) |>
+  CohortConstructor::requirePriorObservation(
+    minPriorObservation = 365
+  ) |>
+  CohortConstructor::requireCohortIntersect(
+    targetCohortTable = "all_mi_events",
+    window = c(-28, -1),
+    intersections = 0,
+    targetEndDate = NULL
+  ) |>
+  CohortConstructor::requireIsFirstEntry()
 
 # Beta-blocker outcome events ----
 cdm$beta_blocker_events <- CohortConstructor::conceptCohort(
